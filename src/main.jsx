88242fbde570073f7c725bd51b985ad610692e82
Fixednow <fixednow@outlook.ie>
2026-07-05 19:17:31 +0100

    Initialize main.jsx with React and ProviderApp

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ProviderApp from './ProviderApp.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ProviderApp />
+  </React.StrictMode>
+);
